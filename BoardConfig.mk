227c1372e6e750716c6b0eaf13dae3a76479d3fe
Flominator <github.flominatorgd@gmail.com>
2023-03-11 01:00:20 +0100

    a3y17lte: unify configs/board/*.mk into BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,9 +28,6 @@ TARGET_INIT_VENDOR_LIB := libinit_a3y17lte
 LOCAL_PATH := device/samsung/a3y17lte
 TARGET_COPY_OUT_VENDOR := system/vendor
 
-# inherit the splitted configs
--include $(LOCAL_PATH)/configs/board/*.mk
-
 # SELinux
 BOARD_SEPOLICY_DIRS += device/samsung/a3y17lte/sepolicy
 
@@ -43,5 +40,52 @@ TARGET_SYSTEM_PROP += $(LOCAL_PATH)/system.prop
 # Inherit from the proprietary version
 -include vendor/samsung/a3y17lte/BoardConfigVendor.mk
 
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+BOARD_HAS_QCA_BT_ROME := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+QCOM_BT_USE_BTNV := true
+QCOM_BT_USE_SMD_TTY := true
 
+# Fingerprint
+TARGET_SEC_FP_HAL_VARIANT := bauth
 
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/exynos7870
+TARGET_KERNEL_CONFIG := lineage-a3y17lte_defconfig
+
+# Partition Sizes
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 33554432
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 39845888
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 3072000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 10737418240
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 209715200
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+# Use dedicated /cache partition instead of /data/cache
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE  := ext4
+
+# Release tools extention
+TARGET_RELEASETOOLS_EXTENSIONS := $(LOCAL_PATH)/releasetools
+
+# Shim
+TARGET_LD_SHIM_LIBS += \
+    /system/lib/libbauthserver.so|/system/vendor/lib/libbauthtzcommon_shim.so
+# Wifi
+BOARD_HAS_QCOM_WLAN              := true
+BOARD_WLAN_DEVICE                := qcwcn
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_AP           := "ap"
+WIFI_DRIVER_FW_PATH_STA          := "sta"
+WIFI_DRIVER_FW_PATH_P2P          := "p2p"
+WIFI_DRIVER_MODULE_NAME          := wlan
+WIFI_DRIVER_MODULE_PATH          := /modules/qca_cld/qca_cld_wlan.ko
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+WPA_SUPPLICANT_USE_HIDL          := true
+
+# Wifi loader
+BOARD_HAVE_SAMSUNG_WIFI := true
